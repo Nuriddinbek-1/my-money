fcc7036736c962d7b8cc4c09c8cd07f017ea6692
Nuriddinbek Obidjonov <nuriddinbek.obidjonov@gmail.com>
2025-09-26 18:17:20 +0500

    firestore initialized

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAq_uDUHgijgAh7YVrj4achvdpk3makyyw",
+  authDomain: "mymoney-b0f6c.firebaseapp.com",
+  projectId: "mymoney-b0f6c",
+  storageBucket: "mymoney-b0f6c.firebasestorage.app",
+  messagingSenderId: "684257561133",
+  appId: "1:684257561133:web:fa4b7338780119b54706a4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Database
+export const db = getFirestore(app);
